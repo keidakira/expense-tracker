cc653fc6b50b7944918bb4f19afc201557aa0b99
Srinandan Komanduri <keida1412@gmail.com>
2022-07-29 20:22:30 -0700

    [Server] Added console.log to errorHandler.js file

--- server/helpers/errorHandler.js
+++ server/helpers/errorHandler.js
@@ -4,6 +4,7 @@
  * This file handles all sorts of errors that may occur in the application.
  */
 const generateErrorMessageFromModelError = ({ errors }) => {
+  console.log("Errors are: ", errors);
   let errorMessages = [];
   for ([key, value] of Object.entries(errors)) {
     errorMessages.push({ name: key, message: value.message });
@@ -13,6 +14,7 @@ const generateErrorMessageFromModelError = ({ errors }) => {
 };
 
 const generateErrorMessageFromJoiError = ({ details }) => {
+  console.log("Error details are: ", details);
   let errorMessages = [];
   // TODO: sometimes single name has multiple messages, allow only one
   for (let i = 0; i < details.length; i++) {
